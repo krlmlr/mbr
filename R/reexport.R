7bf174748d18f94a225e211c7dd0c0aa2ea3ea62
Kirill Müller <krlmlr+r@mailbox.org>
2023-05-28 22:00:42 +0200

    Reexport all of embr

---
+++ R/reexport.R
@@ -0,0 +1,248 @@
+#' @import embr
+NULL
+
+#' @export
+embr::"new_expr<-"
+
+#' @export
+embr::"template<-"
+
+#' @export
+embr::IC
+
+#' @export
+embr::R2
+
+#' @export
+embr::add_analyses
+
+#' @export
+embr::add_models
+
+#' @export
+embr::analyse
+
+#' @export
+embr::analyse1
+
+#' @export
+embr::analyse_residuals
+
+#' @export
+embr::analyses
+
+#' @export
+embr::as.analyses
+
+#' @export
+embr::as.model
+
+#' @export
+embr::as.models
+
+#' @export
+embr::augment
+
+#' @export
+embr::backwards
+
+#' @export
+embr::base_model
+
+#' @export
+embr::check_mb_analysis
+
+#' @export
+embr::check_mb_code
+
+#' @export
+embr::check_mb_model
+
+#' @export
+embr::check_model_pars
+
+#' @export
+embr::check_uniquely_named_list
+
+#' @export
+embr::code
+
+#' @export
+embr::coef
+
+#' @export
+embr::coef_profile
+
+#' @export
+embr::comment_string
+
+#' @export
+embr::data_set
+
+#' @export
+embr::drop_pars
+
+#' @export
+embr::elapsed
+
+#' @export
+embr::estimates
+
+#' @export
+embr::fitted
+
+#' @export
+embr::glance
+
+#' @export
+embr::is.lmb_analysis
+
+#' @export
+embr::is.lmb_code
+
+#' @export
+embr::is.lmb_model
+
+#' @export
+embr::is.mb_analyses
+
+#' @export
+embr::is.mb_analysis
+
+#' @export
+embr::is.mb_code
+
+#' @export
+embr::is.mb_model
+
+#' @export
+embr::is.mb_models
+
+#' @export
+embr::is.mb_null_analysis
+
+#' @export
+embr::is.syntactic
+
+#' @export
+embr::is_bayesian
+
+#' @export
+embr::is_frequentist
+
+#' @export
+embr::is_namedlist
+
+#' @export
+embr::load_model
+
+#' @export
+embr::logLik
+
+#' @export
+embr::log_odds_ratio2
+
+#' @export
+embr::make_all_models
+
+#' @export
+embr::mb_code
+
+#' @export
+embr::mcmc_derive_data
+
+#' @export
+embr::model
+
+#' @export
+embr::models
+
+#' @export
+embr::modify_data
+
+#' @export
+embr::modify_new_data
+
+#' @export
+embr::monitor
+
+#' @export
+embr::new_expr
+
+#' @export
+embr::ngens
+
+#' @export
+embr::nmodels
+
+#' @export
+embr::nthin
+
+#' @export
+embr::odds_ratio2
+
+#' @export
+embr::plot_data
+
+#' @export
+embr::plot_residuals
+
+#' @export
+embr::posterior_predictive_check
+
+#' @export
+embr::predict
+
+#' @export
+embr::proportional_change2
+
+#' @export
+embr::proportional_difference2
+
+#' @export
+embr::random_effects
+
+#' @export
+embr::reanalyse
+
+#' @export
+embr::reanalyse1
+
+#' @export
+embr::rm_comments
+
+#' @export
+embr::sample_size
+
+#' @export
+embr::scalar_nlist
+
+#' @export
+embr::sd_priors_by
+
+#' @export
+embr::select_rescale_data
+
+#' @export
+embr::set_analysis_mode
+
+#' @export
+embr::simulate_residuals
+
+#' @export
+embr::sort_by_ic
+
+#' @export
+embr::sort_nlist
+
+#' @export
+embr::template
+
+#' @export
+embr::terms
+
+#' @export
+embr::tidy
+
+#' @export
+embr::update_model
